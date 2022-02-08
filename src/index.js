bc0a2d1fdc7ecf667f6d9c6330560f590508acf4
Lucas Bigelow <lukejbigelow@gmail.com>
2022-02-08 15:26:41 -0800

    completed tasks through eight

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const intro = document.querySelector('.intro');
 
 const introImg = document.querySelector('.intro img');
 
+const sneakyLink = document.querySelector('.nav a');
+
 // number one -- 'click'
 
 mainHeader.addEventListener('click', function(event) {
@@ -41,5 +43,33 @@ introImg.addEventListener('dblclick', function(e) {
 doc.addEventListener('keydown', function(event) {
   event.preventDefault();
   doc.classList.toggle('blinding');
-  console.log('ahahaha')
+})
+
+// number five -- 'sneaky link'
+
+sneakyLink.addEventListener('click', e => {
+  e.preventDefault();
+  introImg.src = 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?fit=crop&h=1000&mark=https%3A%2F%2Fassets.imgix.net%2F~text%3Fbg%3D80000000%26txt%3DFree%2BStock%2BPhotos%26txtalign%3Dcenter%26txtclr%3Dfff%26txtfont%3DAvenir-Heavy%26txtpad%3D20%26txtsize%3D120%26w%3D1300&markalign=center%2Cmiddle&txt=pexels.com&txtalign=center&txtclr=eeffffff&txtfont=Avenir-Heavy&txtshad=10&txtsize=60&w=1500';
+})
+
+// number six -- 'drag'
+
+sneakyLink.addEventListener('drag', evt => {
+  evt.preventDefault();
+  sneakyLink.classList.toggle('black');
+})
+
+// number seven -- 'wheel'
+
+doc.addEventListener('wheel', evt => {
+  evt.preventDefault();
+  doc.classList.toggle('make-it-small');
+})
+
+// number eight -- 'dragend'
+
+introImg.addEventListener('dragend', function(event) {
+  event.preventDefault();
+  doc.classList.toggle('make-it-blue');
+  console.log('ahahahaha')
 })
