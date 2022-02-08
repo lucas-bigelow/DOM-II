5e26145b3c229103bc59986f82e39424e9dfe0d7
Lucas Bigelow <lukejbigelow@gmail.com>
2022-02-08 14:47:46 -0800

    added four event handlers and had some fun with it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import './less/index.less'
 
 // Your code goes here!
+
+
+// ten different event listeners below here!
+
+// the stuff in the document I'll be changing
+
+const doc = document.querySelector('body');
+
+const mainHeader = document.querySelector('.main-navigation');
+
+const intro = document.querySelector('.intro');
+
+const introImg = document.querySelector('.intro img');
+
+// number one -- 'click'
+
+mainHeader.addEventListener('click', function(event) {
+  event.preventDefault();
+  mainHeader.classList.toggle('black');
+})
+
+// number two -- 'mouseover'
+
+intro.addEventListener('mouseover', evt => {
+  evt.preventDefault();
+  intro.classList.toggle('change-font');
+})
+
+// number three -- 'dblclick'
+
+introImg.addEventListener('dblclick', function(e) {
+  e.preventDefault();
+  introImg.classList.toggle('resize');
+})
+
+// number four -- 'keydown'
+
+doc.addEventListener('keydown', function(event) {
+  event.preventDefault();
+  doc.classList.toggle('blinding');
+  console.log('ahahaha')
+})
